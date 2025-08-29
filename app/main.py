845b2d395b907057ce1dbe645e9898268878ade2
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-29 08:54:41 +0700

    feat: setup logging and database session management
    initialize logging configuration and implement DatabaseSessionManager for application lifespan management. this enhances the application's startup and shutdown processes, ensuring proper resource handling.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,46 @@
 """fast API entri point."""
 
+from contextlib import asynccontextmanager
+from pathlib import Path
+
 from fastapi import FastAPI
+from loguru import logger
 
+from app.config import get_settings
+from app.database import DatabaseSessionManager
 from app.exception import register_exception_handlers
+from app.utils.mlogging.setup import setup_logging
 
-app = FastAPI()
+# Setup settings and logging
+settings = get_settings()
+logconfigpath = Path(__file__).parent.parent / "config_log.yaml"
+setup_logging(config_path=logconfigpath, env=settings.APP.environment.value)
 
+
+# Setup DatabaseSessionManager for lifespan
+sessionmanager = DatabaseSessionManager(settings.DB.url)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Application starting up.")
+    yield
+    logger.info("Application shutting down.")
+    await sessionmanager.close()
+
+
+app = FastAPI(
+    title=settings.APP.name,
+    version=settings.APP.version,
+    debug=settings.APP.debug,
+    description="aplikasi untuk helper parsing reply addon json yang panjang panjang",
+    lifespan=lifespan,
+)
+
+# 3. Registrasi exception handler
 register_exception_handlers(app)
+
+
+@app.get("/")
+async def root():  # noqa: D103
+    return {"message": "Hello World"}
